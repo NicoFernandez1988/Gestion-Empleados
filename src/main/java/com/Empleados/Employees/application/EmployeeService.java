c5eb00b6bfe9528fe13cbd465d135af13ffcd58a
Nico <you@example.com>
2024-08-09 13:10:20 -0300

    try-catch added to EmployeeService class

--- src/main/java/com/Empleados/Employees/application/EmployeeService.java
+++ src/main/java/com/Empleados/Employees/application/EmployeeService.java
@@ -38,61 +38,100 @@ public class EmployeeService implements EmployeeInputPort {
     @Override
     public Employee saveEmployee(EmployeeDTO employeeDTO) throws EmployeeNotFoundException, EmployeeDuplicateException {
         logger.info("Creating employee with name: {}", employeeDTO.getName());
-        Employee employee = convertToEntity(employeeDTO);
-        employeeValidationService.validateEmployeeNameAndLastName(employeeDTO);
-        employeeAssignmentService.assignSupervisor(employeeDTO);
-        employeeAssignmentService.assignTasks(employeeDTO);
-        return employeeRepositoryOutputPort.save(employee);
+        try {
+            Employee employee = convertToEntity(employeeDTO);
+            employeeValidationService.validateEmployeeNameAndLastName(employeeDTO);
+            employeeAssignmentService.assignSupervisor(employeeDTO);
+            employeeAssignmentService.assignTasks(employeeDTO);
+            return employeeRepositoryOutputPort.save(employee);
+        }catch (EmployeeNotFoundException e) {
+            logger.error("Employee not found during the save operation: {}", e.getMessage());
+            throw e;
+        } catch (EmployeeDuplicateException e) {
+            logger.error("Duplicate employee detected during the save operation: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            logger.error("An unexpected error occurred during the save operation: {}", e.getMessage());
+            throw new RuntimeException("Unexpected error occurred while saving employee", e);
+        }
+
     }
 
     @Override
     public List<Employee> getAllEmployees() {
         logger.info("Fetching all employees");
-        return employeeRepositoryOutputPort.findAll(Sort.by("name", "lastName"));
+        try {
+            return employeeRepositoryOutputPort.findAll(Sort.by("name", "lastName"));
+        }catch (Exception e){
+            logger.error("Error fetching employees. {}", e.getMessage());
+            throw e;
+        }
     }
 
     @Override
     public Employee getEmployeeById(Long id) throws EmployeeNotFoundException {
         logger.info("Fetching employee with id: {}", id);
-         employeeRepositoryOutputPort.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
-        return employeeRepositoryOutputPort.findById(id).orElse(null);
+        try {
+            employeeRepositoryOutputPort.findById(id)
+                    .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+            return employeeRepositoryOutputPort.findById(id).orElse(null);
+        }catch (EmployeeNotFoundException e){
+            logger.error("Error fetching employee. {}", e.getMessage());
+            throw e;
+        }
     }
 
     @Override
     public Employee updateEmployee(Long id, EmployeeDTO employeeDTO) throws EmployeeNotFoundException, EmployeeDuplicateException  {
         logger.info("Updating employee with id: {}", id);
-        Employee existingEmployee = employeeRepositoryOutputPort.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+        try {
+            Employee existingEmployee = employeeRepositoryOutputPort.findById(id)
+                    .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
 
-        Employee employee = convertToEntity(employeeDTO);
-        employee.setId(id);
-        employeeDTO.setId(id);
-        employeeValidationService.validateEmployeeNameAndLastName(employeeDTO);
-        employeeAssignmentService.assignSupervisor(employeeDTO);
-        employeeAssignmentService.assignTasks(employeeDTO);
+            Employee employee = convertToEntity(employeeDTO);
+            employee.setId(id);
+            employeeDTO.setId(id);
+            employeeValidationService.validateEmployeeNameAndLastName(employeeDTO);
+            employeeAssignmentService.assignSupervisor(employeeDTO);
+            employeeAssignmentService.assignTasks(employeeDTO);
 
-        existingEmployee.setName(employee.getName());
-        existingEmployee.setLastName(employee.getLastName());
-        existingEmployee.setCellPhone(employee.getCellPhone());
-        existingEmployee.setGender(employee.getGender());
-        existingEmployee.setSupervisor(employee.getSupervisor());
-        existingEmployee.setTasks(employee.getTasks());
+            existingEmployee.setName(employee.getName());
+            existingEmployee.setLastName(employee.getLastName());
+            existingEmployee.setCellPhone(employee.getCellPhone());
+            existingEmployee.setGender(employee.getGender());
+            existingEmployee.setSupervisor(employee.getSupervisor());
+            existingEmployee.setTasks(employee.getTasks());
 
-        return employeeRepositoryOutputPort.save(existingEmployee);
+            return employeeRepositoryOutputPort.save(existingEmployee);
+        }catch (EmployeeNotFoundException e) {
+            logger.error("Employee not found during the update operation: {}", e.getMessage());
+            throw e;
+        } catch (EmployeeDuplicateException e) {
+            logger.error("Duplicate employee detected during the update operation: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            logger.error("An unexpected error occurred during the update operation: {}", e.getMessage());
+            throw new RuntimeException("Unexpected error occurred while saving employee", e);
+        }
+
     }
 
     @Override
     public void deleteEmployee(Long id) throws EmployeeNotFoundException {
         logger.info("Deleting employee with id: {}", id);
-        Employee employee = employeeRepositoryOutputPort.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found "));
-        List<Employee> supervisedEmployees = employeeRepositoryOutputPort.findBySupervisor(employee);
-        for (Employee e : supervisedEmployees) {
-            e.setSupervisor(null);
-            employeeRepositoryOutputPort.save(e);
+        try {
+            Employee employee = employeeRepositoryOutputPort.findById(id)
+                    .orElseThrow(() -> new EmployeeNotFoundException("Employee not found "));
+            List<Employee> supervisedEmployees = employeeRepositoryOutputPort.findBySupervisor(employee);
+            for (Employee e : supervisedEmployees) {
+                e.setSupervisor(null);
+                employeeRepositoryOutputPort.save(e);
+            }
+            employeeRepositoryOutputPort.delete(employee);
+        }catch (EmployeeNotFoundException e){
+            logger.error("Error deleting employee. {}", e.getMessage());
+            throw e;
         }
-        employeeRepositoryOutputPort.delete(employee);
     }
 
     public Employee convertToEntity(EmployeeDTO employeeDTO) {
